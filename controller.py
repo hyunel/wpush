22cf32f5a53072e718ee61cce3e82e5c7df1ff11
thund1r <thuncoder@foxmail.com>
2022-04-25 02:54:12 +0800

    增加图文推送，修改参数名称

--- controller.py
+++ controller.py
@@ -7,9 +7,17 @@ import config
 from wx_api import WxApi
 from errors import *
 import render
+import requests
 
 DB = Db() if config.get('db_addr') != '' and config.get('db_name') != '' else None
 WX_API = WxApi()
+
+
+def get_bing():
+    bing_url = "https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN"
+    res = requests.get(bing_url).json()
+    bing_pic = "https://cn.bing.com/"+res["images"][0]["url"]
+    return bing_pic
 
 
 class MainController:
@@ -69,44 +77,66 @@ class MainController:
 
     def send_msg(self):
         # TODO 更多消息类型的支持
-        self.verify_params('tittle')
-        tittle = self.get_param('tittle')
+        type = self.get_param('type')
+        title = self.get_param('title')
         content = self.get_param('content')
+        pic = self.get_param('pic')
         summary = self.get_param('summary')
+        if not type:
+            type = "text"
         if not content:
-            WX_API.send_text(self.get_param('tittle'), **self.spec_send_to())
-        else:
-            if not summary:
-                summary = content[:128] + '...' if len(content) > 128 else content
-            # 数据库已经正常配置
-            if DB:
-                msg = DB.insert_message(tittle, content)
-                WX_API.send_text_card(msg.tittle, summary, '{}/show/{}'.format(config.get('sys_url'), msg.safe_id),
+            content = "No Content"
+        if not summary:
+            summary = content[:128] + '...' if len(content) > 128 else content
+        if not pic and type == "news":
+            pic = get_bing()
+
+        if DB:
+            msg = DB.insert_message(title, content)
+            if type == "text":
+                WX_API.send_text(msg.content, **self.spec_send_to())
+            elif type == "markdown":
+                WX_API.send_markdown(msg.content, **self.spec_send_to())
+            elif type == "textcard":
+                WX_API.send_text_card(msg.title, summary, '{}/show/{}'.format(config.get('sys_url'), msg.safe_id),
                                       **self.spec_send_to())
-                return {"body": {"code": 0, "msg_id": msg.safe_id}}
-            else:
-                # content 放进 url 参数里, 长度超过 1000 则截断
-                WX_API.send_text_card(tittle, summary, '{}/show?t={}&h={}&c={}'.format(
+            elif type == "news":
+                WX_API.send_news(msg.title, summary, '{}/show/{}'.format(config.get('sys_url'), msg.safe_id), pic,
+                                 **self.spec_send_to())
+            return {"body": {"code": 0, "msg_id": msg.safe_id}}
+        else:
+            if type == "text":
+                self.verify_params('content')
+                WX_API.send_text(content, **self.spec_send_to())
+            elif type == "markdown":
+                self.verify_params('content')
+                WX_API.send_markdown(content, **self.spec_send_to())
+            elif type == "textcard":
+                self.verify_params('title')
+                self.verify_params('content')
+                WX_API.send_text_card(title, summary, '{}/show?t={}&h={}&c={}'.format(
                     config.get('sys_url'),
                     int(time.time()*1000),
-                    quote(tittle, encoding='utf-8'),
+                    quote(title, encoding='utf-8'),
                     quote(content, encoding='utf-8')[:1900]), **self.spec_send_to())
-        return {"body": {"code": 0}}
+            elif type == "news":
+                self.verify_params('title')
+                WX_API.send_news(title, summary, '{}/show?t={}&h={}&c={}'.format(
+                    config.get('sys_url'),
+                    int(time.time()*1000),
+                    quote(title, encoding='utf-8'),
+                    quote(content, encoding='utf-8')[:1900]), pic, **self.spec_send_to())
+            return {"body": {"code": 0}}
 
     def show_msg(self):
         if not DB:
             raise BadRequestError("请先配置数据库")
         msg = DB.query_message(self.route_result['params']['msg_id'])
         if msg:
-            return {"body": render.show(msg.tittle, msg.content, msg.time.strftime("%Y-%m-%d %H:%M:%S"))}
+            return {"body": render.show(msg.title, msg.content, msg.time.strftime("%Y-%m-%d %H:%M:%S"))}
         else:
             raise BadRequestError("没有这篇文章")
 
     def show_msg_param(self):
         self.verify_params('t', 'h', 'c')
         return {"body": render.show_param()}
-
-    def send_rich_msg(self):
-        self.verify_params('type', 'data')
-        WX_API.send_msg(self.get_param('type'), self.get_param('data'), **self.spec_send_to())
-        return {"body": {"code": 0}}
