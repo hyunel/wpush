7eb1a54c0a15c0fd189eace207d4be9716d98491
Hyun <hyunsssssssss@gmail.com>
2023-10-21 16:54:34 +0800

    🎉 添加阿里云函数计算支持

--- index.py
+++ index.py
@@ -1,10 +1,52 @@
 import json
+from urllib.parse import parse_qs
 
-import route
 import controller
+import route
 from errors import YunError
 
 route = route.setup_routes()
+
+
+# 阿里云 HTTP 触发器
+def handler(environ, start_response):
+    route_result = route.match(environ['fc.request_uri'], environ['REQUEST_METHOD'])
+    if route_result is None:
+        start_response('404 Not Found', [])
+        return []
+
+    try:
+        request_body_size = int(environ.get('CONTENT_LENGTH', 0))
+        request_body = environ['wsgi.input'].read(request_body_size)
+        if type(request_body) is bytes:
+            request_body = request_body.decode(encoding='utf8')
+    except ValueError:
+        request_body = None
+
+    try:
+        query_string = parse_qs(environ['QUERY_STRING'])
+        for key in query_string:
+            if len(query_string[key]) == 1:
+                query_string[key] = query_string[key][0]
+
+        obj = controller.MainController({
+            'body': request_body,
+            'queryString': query_string
+        }, route_result)
+        result = getattr(obj, route_result['action'])()
+    except YunError as e:
+        start_response('500 Internal Server Error', [('Content-type', 'application/json;charset=utf-8')])
+        return [json.dumps({"code": e.code, "msg": e.message}, ensure_ascii=False)]
+
+    if 'body' in result and type(result['body']) is str:
+        start_response('200 OK', [('Content-type', 'text/html;charset=utf-8')])
+        return [result['body']]
+
+    resp = {"code": 0, "msg": None}
+    start_response('200 OK', [('Content-type', 'application/json;charset=utf-8')])
+    if 'body' in result and type(result['body']) is dict:
+        resp.update(result['body'])
+    return [json.dumps(resp, ensure_ascii=False)]
 
 
 # 腾讯 API 网关触发器
